0e2b8385b7bbcd84e193ef292d79fda789792627
bunbunbun777 <51977345+bunbunbun777@users.noreply.github.com>
2024-11-13 20:01:38 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+// Add any interactivity here if needed
+document.addEventListener('DOMContentLoaded', function() {
+    // Example: Adding a console log
+    console.log("Bunardi's Portfolio Loaded");
+});
